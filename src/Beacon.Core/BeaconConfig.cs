7f2f2fb6449a70515c30f2e5b790c17b30e91d97
gljohn <gljohn@fedoraproject.org>
2017-07-12 21:28:12 +0200

    - tidy up fs structre. - start creating new projects - update Beacon.Lib.Beacon to use new solution structure. - update Beacon.Lib.Beacon to compile under dotnetcore1.1

---
+++ src/Beacon.Core/BeaconConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace Beacon.Core
+{
+    public class BeaconConfig
+    {
+        /// <summary>
+        /// Return the machine's hostname (usually nice to mention in the beacon text)
+        /// </summary>
+        public static string HostName
+        {
+            get { return Dns.GetHostName(); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string BeaconType { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ushort AdvertisedPort { get; private set; }
+
+       
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string BeaconData { get; set; }
+    }
+}
---
+++ src/Beacon.Lib/DatagramPacket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Beacon.Lib
+{
+    internal class DatagramPacket {
+
+        private object _data;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="data"></param>
+        public DatagramPacket(object data) {
+            this._data = data;          
+        }
+
+        /// <summary>
+        /// Convert a string to network bytes
+        /// </summary>
+        public IEnumerable<byte> Encode() {
+            var bytes = Encoding.UTF8.GetBytes((string)this._data);
+            var len = IPAddress.HostToNetworkOrder((short)bytes.Length);
+
+            return BitConverter.GetBytes(len).Concat(bytes);
+        }
+
+        /// <summary>
+        /// Convert network bytes to a string
+        /// </summary>
+        public string Decode() {
+            var listData = this._data as IList<byte> ?? ((IEnumerable<byte>)this._data).ToList();
+
+            var len = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(listData.Take(2).ToArray(), 0));
+            if (listData.Count() < 2 + len) throw new ArgumentException("Too few bytes in packet");
+
+            return Encoding.UTF8.GetString(listData.Skip(2).Take(len).ToArray());
+        }
+
+    }
+}
